01c96773f4f1eed8a7c2052180b8beb16f3c419a
huangli <areyouok@gmail.com>
2022-11-07 10:29:06 +0800

    add JavaVersion

---
+++ client/src/test/java/com/github/dtprj/dongting/common/JavaVersionTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright The Dongting Project
+ *
+ * The Dongting Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.dtprj.dongting.common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author huangli
+ */
+public class JavaVersionTest {
+    @Test
+    public void testMajorVersion() {
+        assertEquals(8, JavaVersion.majorVersion("1.8"));
+        assertEquals(8, JavaVersion.majorVersion("8"));
+        assertEquals(9, JavaVersion.majorVersion("1.9")); // early version of JDK 9 before Project Verona
+        assertEquals(9, JavaVersion.majorVersion("9"));
+    }
+}
