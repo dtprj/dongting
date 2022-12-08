61370e9419eb6f9a7edc43e0a33146c711226165
huangli <areyouok@gmail.com>
2022-12-08 16:45:07 +0800

    JPMS support

---
+++ client-java11/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module dongting.client.java11 {
+    requires dongting.client;
+    exports com.github.dtprj.dongting.common.j11;
+}
---
+++ client/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module dongting.client {
+    requires static java.logging;
+    requires static org.slf4j;
+    requires com.carrotsearch.hppc;
+    requires jctools.core;
+    exports com.github.dtprj.dongting.buf;
+    exports com.github.dtprj.dongting.common;
+    exports com.github.dtprj.dongting.log;
+    exports com.github.dtprj.dongting.net;
+    exports com.github.dtprj.dongting.pb;
+    exports com.github.dtprj.dongting.raft.client;
+}
