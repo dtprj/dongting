f3604556aa38c03cf8e32c339ed19697aed7f130
huangli <areyouok@gmail.com>
2023-06-29 10:12:22 +0800

    feat: add bool encoder/decoder

---
+++ client/src/main/java/com/github/dtprj/dongting/codec/BoolEncoder.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright The Dongting Project
+ *
+ * The Dongting Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.dtprj.dongting.codec;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author huangli
+ */
+public class BoolEncoder implements Encoder<Boolean> {
+
+    public static final BoolEncoder INSTANCE = new BoolEncoder();
+
+    private BoolEncoder() {
+    }
+
+    @Override
+    public boolean encode(EncodeContext context, ByteBuffer buffer, Boolean data) {
+        if (buffer.hasRemaining()) {
+            buffer.put((byte) (data == null ? -1 : data ? 1 : 0));
+        }
+        return false;
+    }
+
+    @Override
+    public int actualSize(Boolean data) {
+        return 1;
+    }
+}
