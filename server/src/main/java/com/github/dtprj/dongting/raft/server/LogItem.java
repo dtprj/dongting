e893e5c6c3f0893004067d174221de3db9304d06
huangli <areyouok@gmail.com>
2023-10-28 22:18:03 +0800

    feat: raft impl v2, part 1

---
+++ server/src/main/java/com/github/dtprj/dongting/raft/server/LogItem.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright The Dongting Project
+ *
+ * The Dongting Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.dtprj.dongting.raft.server;
+
+import com.github.dtprj.dongting.buf.ByteBufferPool;
+import com.github.dtprj.dongting.common.RefCount;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author huangli
+ */
+public class LogItem extends RefCount {
+    public static final int TYPE_NORMAL = 0;
+    public static final int TYPE_HEARTBEAT = 1;
+    public static final int TYPE_PREPARE_CONFIG_CHANGE = 2;
+    public static final int TYPE_DROP_CONFIG_CHANGE = 3;
+    public static final int TYPE_COMMIT_CONFIG_CHANGE = 4;
+
+    private final ByteBufferPool heapPool;
+
+    private int type;
+    private int bizType;
+    private int term;
+    private long index;
+    private int prevLogTerm;
+    private long timestamp;
+
+    private ByteBuffer bodyBuffer;
+    private Object body;
+    private int actualBodySize;
+
+    private ByteBuffer headerBuffer;
+    private Object header;
+    private int actualHeaderSize;
+
+    private int pbHeaderSize;
+    private int pbItemSize;
+
+    public LogItem(ByteBufferPool heapPool) {
+        super(true);
+        this.heapPool = heapPool;
+    }
+
+    @Override
+    protected void doClean() {
+        if (headerBuffer != null && heapPool != null) {
+            heapPool.release(headerBuffer);
+        }
+        if (bodyBuffer != null && heapPool != null) {
+            heapPool.release(bodyBuffer);
+        }
+        if (header instanceof RefCount) {
+            ((RefCount) header).release();
+        }
+        if (body instanceof RefCount) {
+            ((RefCount) body).release();
+        }
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getBizType() {
+        return bizType;
+    }
+
+    public void setBizType(int bizType) {
+        this.bizType = bizType;
+    }
+
+    public int getTerm() {
+        return term;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public int getPrevLogTerm() {
+        return prevLogTerm;
+    }
+
+    public void setPrevLogTerm(int prevLogTerm) {
+        this.prevLogTerm = prevLogTerm;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    public int getActualBodySize() {
+        return actualBodySize;
+    }
+
+    public void setActualBodySize(int actualBodySize) {
+        this.actualBodySize = actualBodySize;
+    }
+
+    public long getIndex() {
+        return index;
+    }
+
+    public void setIndex(long index) {
+        this.index = index;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public ByteBuffer getBodyBuffer() {
+        return bodyBuffer;
+    }
+
+    public void setBodyBuffer(ByteBuffer bodyBuffer) {
+        this.bodyBuffer = bodyBuffer;
+    }
+
+    public Object getHeader() {
+        return header;
+    }
+
+    public void setHeader(Object header) {
+        this.header = header;
+    }
+
+    public int getActualHeaderSize() {
+        return actualHeaderSize;
+    }
+
+    public void setActualHeaderSize(int actualHeaderSize) {
+        this.actualHeaderSize = actualHeaderSize;
+    }
+
+    public ByteBuffer getHeaderBuffer() {
+        return headerBuffer;
+    }
+
+    public void setHeaderBuffer(ByteBuffer headerBuffer) {
+        this.headerBuffer = headerBuffer;
+    }
+
+    public int getPbHeaderSize() {
+        return pbHeaderSize;
+    }
+
+    public void setPbHeaderSize(int pbHeaderSize) {
+        this.pbHeaderSize = pbHeaderSize;
+    }
+
+    public int getPbItemSize() {
+        return pbItemSize;
+    }
+
+    public void setPbItemSize(int pbItemSize) {
+        this.pbItemSize = pbItemSize;
+    }
+}
---
+++ server/src/main/java/com/github/dtprj/dongting/raft/server/RaftGroupConfig.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright The Dongting Project
+ *
+ * The Dongting Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.dtprj.dongting.raft.server;
+
+import com.github.dtprj.dongting.buf.ByteBufferPool;
+import com.github.dtprj.dongting.buf.RefBufferFactory;
+import com.github.dtprj.dongting.common.Timestamp;
+import com.github.dtprj.dongting.raft.sm.RaftCodecFactory;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.function.Supplier;
+
+/**
+ * @author huangli
+ */
+public class RaftGroupConfig {
+    private final int groupId;
+    private final String nodeIdOfMembers;
+    private final String nodeIdOfObservers;
+    private String dataDir = "./data";
+    private String statusFile = "raft.status";
+    private int[] ioRetryInterval = new int[]{100, 1000, 3000, 5000, 10000, 20000};
+
+    private Timestamp ts;
+    private RefBufferFactory heapPool;
+    private ByteBufferPool directPool;
+    private ExecutorService ioExecutor;
+    private Executor raftExecutor;
+    private Supplier<Boolean> stopIndicator;
+    private RaftStatus raftStatus;
+    private RaftCodecFactory codecFactory;
+
+    public RaftGroupConfig(int groupId, String nodeIdOfMembers, String nodeIdOfObservers) {
+        this.groupId = groupId;
+        this.nodeIdOfMembers = nodeIdOfMembers;
+        this.nodeIdOfObservers = nodeIdOfObservers;
+    }
+
+    public String getNodeIdOfMembers() {
+        return nodeIdOfMembers;
+    }
+
+    public String getDataDir() {
+        return dataDir;
+    }
+
+    public void setDataDir(String dataDir) {
+        this.dataDir = dataDir;
+    }
+
+    public String getStatusFile() {
+        return statusFile;
+    }
+
+    public void setStatusFile(String statusFile) {
+        this.statusFile = statusFile;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+
+    public String getNodeIdOfObservers() {
+        return nodeIdOfObservers;
+    }
+
+    public Timestamp getTs() {
+        return ts;
+    }
+
+    public void setTs(Timestamp ts) {
+        this.ts = ts;
+    }
+
+    public RefBufferFactory getHeapPool() {
+        return heapPool;
+    }
+
+    public void setHeapPool(RefBufferFactory heapPool) {
+        this.heapPool = heapPool;
+    }
+
+    public ByteBufferPool getDirectPool() {
+        return directPool;
+    }
+
+    public void setDirectPool(ByteBufferPool directPool) {
+        this.directPool = directPool;
+    }
+
+    public Executor getRaftExecutor() {
+        return raftExecutor;
+    }
+
+    public void setRaftExecutor(Executor raftExecutor) {
+        this.raftExecutor = raftExecutor;
+    }
+
+    public Supplier<Boolean> getStopIndicator() {
+        return stopIndicator;
+    }
+
+    public void setStopIndicator(Supplier<Boolean> stopIndicator) {
+        this.stopIndicator = stopIndicator;
+    }
+
+    public RaftStatus getRaftStatus() {
+        return raftStatus;
+    }
+
+    public void setRaftStatus(RaftStatus raftStatus) {
+        this.raftStatus = raftStatus;
+    }
+
+    public RaftCodecFactory getCodecFactory() {
+        return codecFactory;
+    }
+
+    public void setCodecFactory(RaftCodecFactory codecFactory) {
+        this.codecFactory = codecFactory;
+    }
+
+    public ExecutorService getIoExecutor() {
+        return ioExecutor;
+    }
+
+    public void setIoExecutor(ExecutorService ioExecutor) {
+        this.ioExecutor = ioExecutor;
+    }
+
+    public int[] getIoRetryInterval() {
+        return ioRetryInterval;
+    }
+
+    public void setIoRetryInterval(int[] ioRetryInterval) {
+        this.ioRetryInterval = ioRetryInterval;
+    }
+}
