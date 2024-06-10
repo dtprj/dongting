faf5878bd09b93ccc4fb659b8c9bbcf657c48f0d
huangli <areyouok@gmail.com>
2024-06-10 18:17:52 +0800

    bench: merge raft benchmark

--- benchmark/src/main/java/com/github/dtprj/dongting/bench/raft/RaftBenchmark.java
+++ benchmark/src/main/java/com/github/dtprj/dongting/bench/raft/RaftBenchmark.java
@@ -19,7 +19,6 @@ import com.github.dtprj.dongting.bench.common.BenchBase;
 import com.github.dtprj.dongting.bench.common.SimplePerfCallback;
 import com.github.dtprj.dongting.common.DtTime;
 import com.github.dtprj.dongting.common.DtUtil;
-import com.github.dtprj.dongting.common.Pair;
 import com.github.dtprj.dongting.common.PerfCallback;
 import com.github.dtprj.dongting.dtkv.KvClient;
 import com.github.dtprj.dongting.dtkv.server.DtKV;
@@ -39,7 +38,6 @@ import com.github.dtprj.dongting.raft.server.RaftServerConfig;
 import com.github.dtprj.dongting.raft.sm.StateMachine;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -49,11 +47,12 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author huangli
  */
-public class Raft3Benchmark extends BenchBase {
-    private static final DtLog log = DtLogs.getLogger(Raft3Benchmark.class);
+public class RaftBenchmark extends BenchBase {
+    private static final DtLog log = DtLogs.getLogger(RaftBenchmark.class);
     private static final String DATA_DIR = "target/raftlog";
     private static final int GROUP_ID = 0;
 
+    private static final int NODE_COUNT = 3;
     private static final boolean SYNC = false;
     private static final int DATA_LEN = 256;
     private static final byte[] DATA = new byte[DATA_LEN];
@@ -62,23 +61,22 @@ public class Raft3Benchmark extends BenchBase {
     private static final boolean SYNC_FORCE = true;
     private static final int KEYS = 100_000;
 
-    private final RaftServer[] raftServers = new RaftServer[3];
+    private final List<RaftServer> raftServers = new ArrayList<>();
     private final List<RaftGroupConfig> groupConfigs = new ArrayList<>();
-    private final DefaultRaftFactory[] raftFactories = new DefaultRaftFactory[3];
-    private KvClient[] client;
+    private final List<DefaultRaftFactory> raftFactories = new ArrayList<>();
+    private KvClient[] clients;
 
     public static void main(String[] args) throws Exception {
-        Raft3Benchmark benchmark = new Raft3Benchmark(1, 10000, 200);
+        RaftBenchmark benchmark = new RaftBenchmark(1, 10000, 200);
         benchmark.setLogRt(true);
         benchmark.start();
     }
 
-    public Raft3Benchmark(int threadCount, long testTime, long warmupTime) {
+    public RaftBenchmark(int threadCount, long testTime, long warmupTime) {
         super(threadCount, testTime, warmupTime);
     }
 
-    private Pair<RaftServer, DefaultRaftFactory> createServer(int nodeId, int replicatePort, int servicePort,
-                                                              String servers, String nodeIdOfMembers) {
+    private void createServer(int nodeId, int replicatePort, int servicePort, String servers, String nodeIdOfMembers) {
         RaftServerConfig serverConfig = new RaftServerConfig();
         serverConfig.setServers(servers);
         serverConfig.setNodeId(nodeId);
@@ -94,8 +92,18 @@ public class Raft3Benchmark extends BenchBase {
             groupConfig.setPerfCallback(new RaftPerfCallback(true, "node" + nodeId + "_"));
         }
 
-        groupConfigs.add(groupConfig);
+        DefaultRaftFactory raftFactory = createRaftFactory(nodeId, serverConfig);
 
+        RaftServer raftServer = new RaftServer(serverConfig, Collections.singletonList(groupConfig), raftFactory);
+        KvServerUtil.initKvServer(raftServer);
+        raftServer.start();
+
+        groupConfigs.add(groupConfig);
+        raftServers.add(raftServer);
+        raftFactories.add(raftFactory);
+    }
+
+    private DefaultRaftFactory createRaftFactory(int nodeId, RaftServerConfig serverConfig) {
         DefaultRaftFactory raftFactory = new DefaultRaftFactory(serverConfig) {
             @Override
             public StateMachine createStateMachine(RaftGroupConfigEx groupConfig) {
@@ -104,61 +112,54 @@ public class Raft3Benchmark extends BenchBase {
 
             @Override
             public FiberGroup createFiberGroup(RaftGroupConfig groupConfig) {
-                // we start 3 node in same jvm, so use node id as part of dispatcher name
+                // we start multi nodes in same jvm, so use node id as part of dispatcher name
                 Dispatcher dispatcher = new Dispatcher("node-" + nodeId, groupConfig.getPerfCallback());
                 dispatcher.start();
                 return new FiberGroup("group-" + GROUP_ID + "-node-" + nodeId, dispatcher);
             }
         };
         raftFactory.start();
-
-        RaftServer raftServer = new RaftServer(serverConfig, Collections.singletonList(groupConfig), raftFactory);
-        KvServerUtil.initKvServer(raftServer);
-        raftServer.start();
-        return new Pair<>(raftServer, raftFactory);
+        return raftFactory;
     }
 
     @Override
     public void init() throws Exception {
         new Random().nextBytes(DATA);
-        String serversStr = "1,127.0.0.1:4001;2,127.0.0.1:4002;3,127.0.0.1:4003";
-        String memberIds = "1,2,3";
-        Pair<RaftServer, DefaultRaftFactory> p1 = createServer(1, 4001, 5001,
-                serversStr, memberIds);
-        Pair<RaftServer, DefaultRaftFactory> p2 = createServer(2, 4002, 5002,
-                serversStr, memberIds);
-        Pair<RaftServer, DefaultRaftFactory> p3 = createServer(3, 4003, 5003,
-                serversStr, memberIds);
-        raftServers[0] = p1.getLeft();
-        raftServers[1] = p2.getLeft();
-        raftServers[2] = p3.getLeft();
-        raftFactories[0] = p1.getRight();
-        raftFactories[1] = p2.getRight();
-        raftFactories[2] = p3.getRight();
+        StringBuilder serversStr = new StringBuilder();
+        StringBuilder memberIds = new StringBuilder();
+        List<RaftNode> serverNodes = new ArrayList<>();
+        for (int i = 1; i <= NODE_COUNT; i++) {
+            serversStr.append(i).append(",127.0.0.1:").append(4000 + i).append(";");
+            memberIds.append(i).append(",");
+            serverNodes.add(new RaftNode(i, new HostPort("127.0.0.1", 5000 + i)));
+        }
+        serversStr.deleteCharAt(serversStr.length() - 1);
+        memberIds.deleteCharAt(memberIds.length() - 1);
+
+        for (int i = 1; i <= NODE_COUNT; i++) {
+            createServer(i, 4000 + i, 5000 + i, serversStr.toString(), memberIds.toString());
+        }
 
         for (RaftServer s : raftServers) {
             s.getAllGroupReadyFuture().get(60, TimeUnit.SECONDS);
         }
         log.info("raft servers started");
 
-        client = new KvClient[threadCount];
+        clients = new KvClient[threadCount];
         for (int i = 0; i < threadCount; i++) {
             NioClientConfig nioClientConfig = new NioClientConfig();
             nioClientConfig.setMaxOutRequests(CLIENT_MAX_OUT_REQUESTS / threadCount);
             KvClient c = new KvClient(nioClientConfig);
             c.start();
-            RaftNode n1 = new RaftNode(1, new HostPort("127.0.0.1", 5001));
-            RaftNode n2 = new RaftNode(2, new HostPort("127.0.0.1", 5002));
-            RaftNode n3 = new RaftNode(3, new HostPort("127.0.0.1", 5003));
-            c.getRaftClient().addOrUpdateGroup(GROUP_ID, Arrays.asList(n1, n2, n3));
-            client[i] = c;
+            c.getRaftClient().addOrUpdateGroup(GROUP_ID, serverNodes);
+            clients[i] = c;
         }
 
         //noinspection rawtypes
         CompletableFuture[] futures = new CompletableFuture[threadCount];
         for (int i = 0; i < threadCount; i++) {
             // make c find the leader
-            futures[i] = client[i].get(GROUP_ID, "kkk", new DtTime(5, TimeUnit.SECONDS));
+            futures[i] = clients[i].get(GROUP_ID, "kkk", new DtTime(5, TimeUnit.SECONDS));
         }
         CompletableFuture.allOf(futures).get();
     }
@@ -176,9 +177,9 @@ public class Raft3Benchmark extends BenchBase {
     @Override
     public void shutdown() {
         DtTime timeout = new DtTime(3, TimeUnit.SECONDS);
-        DtUtil.stop(timeout, client);
-        DtUtil.stop(timeout, raftServers);
-        DtUtil.stop(timeout, raftFactories);
+        DtUtil.stop(timeout, clients);
+        DtUtil.stop(timeout, raftServers.toArray(new RaftServer[0]));
+        DtUtil.stop(timeout, raftFactories.toArray(new DefaultRaftFactory[0]));
 
         for (RaftGroupConfig config : groupConfigs) {
             if (config.getPerfCallback() instanceof RaftPerfCallback) {
@@ -195,7 +196,7 @@ public class Raft3Benchmark extends BenchBase {
             int k = Integer.reverse((int) startTime);
             k = k % KEYS;
             final DtTime timeout = new DtTime(800, TimeUnit.MILLISECONDS);
-            CompletableFuture<Void> f = client[threadIndex].put(GROUP_ID, String.valueOf(k), DATA, timeout);
+            CompletableFuture<Void> f = clients[threadIndex].put(GROUP_ID, String.valueOf(k), DATA, timeout);
 
             if (SYNC) {
                 f.get();
