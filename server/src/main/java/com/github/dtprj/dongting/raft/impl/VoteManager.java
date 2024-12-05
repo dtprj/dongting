04d22d4f4701d88d8038bf5f3eadf50c58e5eaf7
huangli <areyouok@gmail.com>
2024-12-05 23:31:55 +0800

    chore: leader not change to follower after lease timeout

--- server/src/main/java/com/github/dtprj/dongting/raft/impl/VoteManager.java
+++ server/src/main/java/com/github/dtprj/dongting/raft/impl/VoteManager.java
@@ -248,10 +248,10 @@ public class VoteManager {
             if (raftStatus.getRole() == RaftRole.observer) {
                 return sleepToNextElectTime();
             }
-            if (raftStatus.getRole() == RaftRole.leader && raftStatus.getLeaseStartNanos()
-                    + raftStatus.getElectTimeoutNanos() - raftStatus.getTs().getNanoTime() < 0) {
-                RaftUtil.changeToFollower(raftStatus, -1, "leader lease timeout");
-            }
+            //if (raftStatus.getRole() == RaftRole.leader && raftStatus.getLeaseStartNanos()
+            //        + raftStatus.getElectTimeoutNanos() - raftStatus.getTs().getNanoTime() < 0) {
+            //    RaftUtil.changeToFollower(raftStatus, -1, "leader lease timeout");
+            //}
             boolean timeout = raftStatus.getTs().getNanoTime() - raftStatus.getLastElectTime() > raftStatus.getElectTimeoutNanos();
             if (voting) {
                 if (timeout) {
