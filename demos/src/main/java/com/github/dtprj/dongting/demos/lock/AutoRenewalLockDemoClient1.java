810f475c905e2798ae738e9153dec63b7fb834e6
huangli <areyouok@gmail.com>
2025-11-19 21:34:51 +0800

    demo: add lock demos

---
+++ demos/src/main/java/com/github/dtprj/dongting/demos/lock/AutoRenewalLockDemoClient1.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright The Dongting Project
+ *
+ * The Dongting Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.dtprj.dongting.demos.lock;
+
+import com.github.dtprj.dongting.common.DtTime;
+import com.github.dtprj.dongting.demos.base.DemoClientBase;
+import com.github.dtprj.dongting.dtkv.AutoRenewalLock;
+import com.github.dtprj.dongting.dtkv.AutoRenewalLockListener;
+import com.github.dtprj.dongting.dtkv.KvClient;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author huangli
+ */
+public class AutoRenewalLockDemoClient1 extends DemoClientBase implements GroupId {
+
+    static final AutoRenewalLockListener listener = new AutoRenewalLockListener() {
+        @Override
+        public void onAcquired() {
+            System.out.println("lock acquired");
+        }
+
+        @Override
+        public void onLost() {
+            System.out.println("lock lost");
+        }
+    };
+
+    public static void main(String[] args) throws Exception {
+        String servers = "1,127.0.0.1:5001"; // serverId,ip:servicePort
+        KvClient kvClient = new KvClient();
+        kvClient.start();
+        kvClient.getRaftClient().clientAddNode(servers);
+        kvClient.getRaftClient().clientAddOrUpdateGroup(GROUP_ID, new int[]{1});
+
+        AutoRenewalLock lock = kvClient.createAutoRenewalLock(GROUP_ID, "autoRenewalLock1".getBytes(),
+                30000, listener);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            lock.close();
+            kvClient.stop(new DtTime(3, TimeUnit.SECONDS));
+        }));
+    }
+}
