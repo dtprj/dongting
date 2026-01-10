84ebcdc72ca4315a6d3c9fd28864dea35026e66f
huangli <areyouok@gmail.com>
2026-01-10 15:50:12 +0800

    test: move test

--- dist/src/test/java/com/github/dtprj/dongting/dist/PropsUtilTest.java
+++ dist/src/test/java/com/github/dtprj/dongting/dist/PropsUtilTest.java
@@ -13,9 +13,8 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package com.github.dtprj.dongting.dist.boot;
+package com.github.dtprj.dongting.dist;
 
-import com.github.dtprj.dongting.dist.PropsUtil;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
